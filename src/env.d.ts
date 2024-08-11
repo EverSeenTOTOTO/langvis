2cc524297827278acb23caf9d8885c91829a6a19
EverSeenTOTOTO <everseenflash@163.com>
2024-08-11 13:09:06 +0800

    build: init proj

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+declare interface Window {
+  __PREFETCHED_STATE__: any;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_SERVER_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
